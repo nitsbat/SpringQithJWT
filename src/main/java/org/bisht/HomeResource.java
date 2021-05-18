82a42d304b24b190e922a1d45b49e868426c664b
Nitin Bisht <3498georgian@gmail.com>
2021-05-18 12:44:32 +0530

    Added the controller and main class

---
+++ src/main/java/org/bisht/HomeResource.java
@@ -0,0 +1,15 @@
+package org.bisht;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Controller
+public class HomeResource {
+
+    @RequestMapping("/hello")
+    public String home() {
+        return "<h1>Hello World</h1>";
+    }
+
+}
---
+++ src/main/java/org/bisht/SpringMainApp.java
@@ -0,0 +1,11 @@
+package org.bisht;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringMainApp {
+    public static void main(String[] args) {
+        SpringApplication.run(SpringMainApp.class, args);
+    }
+}
